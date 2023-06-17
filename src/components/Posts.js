1e24dd76d2a3d4f003c1de75121223f2630672d9
KierranyC <kierranyc@outlook.com>
2023-06-17 01:26:04 -0500

    attempted adding messages

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 const Posts = ({ token }) => {
     const [posts, setPosts] = useState([])
     const [selectedPost, setSelectedPost] = useState(null)
+    // const [loggedIn, setLoggedIn] = useState(false);
+    // const [content, setContent] = useState('');
 
     useEffect(() => {
         async function fetchPosts() {
@@ -13,6 +15,11 @@ const Posts = ({ token }) => {
             setPosts(data.data.posts)
         }
         fetchPosts()
+        // if (token) {
+        //     setLoggedIn(true);
+        // } else {
+        //     setLoggedIn(false);
+        // }
     }, [])
 
     const handlePostClick = (post) => {
@@ -22,6 +29,10 @@ const Posts = ({ token }) => {
     const handleCloseClick = () => {
         setSelectedPost(null)
     }
+
+    // const handleMessageChange = (event) => {
+    //     setContent(event.target.value);
+    // }
 
     // const postMessage = async (event) => {
     //     event.preventDefault()
@@ -82,6 +93,44 @@ const Posts = ({ token }) => {
                     </div>
                 ))}
         </div>
+
+        // <div>
+        //     {selectedPost && (
+        //         <div>
+        //             <h3>{selectedPost.title}</h3>
+        //             <div>{selectedPost.description}</div>
+        //             <button onClick={handleCloseClick}>Close</button>
+        //             {/* {loggedIn ? (
+        //                 <div className="message-box">
+        //                     <h4>Send a message to the post author:</h4>
+        //                     <form onSubmit={postMessage}>
+        //                         <textarea
+        //                             value={content}
+        //                             onChange={handleMessageChange}
+        //                             placeholder="Enter your message"
+        //                         />
+        //                         <br />
+        //                         <button type="submit">Send message</button>
+        //                     </form>
+        //                 </div>
+        //             ) : (
+        //                 <Link to="/login">Login to send a message.</Link>
+        //             )} */}
+        //         </div>
+        //     )}
+        //     {!selectedPost &&
+        //         posts.map((post) => (
+        //             <div key={post._id} value={post}>
+        //                 <Link
+        //                     to={`/posts/${post._id}`}
+        //                     onClick={() => handlePostClick(post)}
+        //                 >
+        //                     {post.title}
+        //                 </Link>
+        //                 <div>{post.description}</div>
+        //             </div>
+        //         ))}
+        // </div>
     )
 
 }
